0027de58f529febecc79e7577a01a330e646d354
Lionel Mann <lionelmann@gmail.com>
2017-03-17 15:10:42 -0400

    page meta fields

---
+++ custom/themes/lgo/includes/page-meta-fields.php
@@ -0,0 +1,36 @@
+<?php
+
+add_filter( 'rwmb_meta_boxes', 'meta_box_group_accordion_register' );
+function meta_box_group_accordion_register( $meta_boxes ) {
+
+	$prefix = 'rw_';
+	$meta_boxes[] = array(
+		'title'  => __( 'More Info - Accordion Format' ),
+		'post_types' => array('page' ),
+		'fields' => array(
+			array(
+				'id'     => 'accordion_content',
+				'type'   => 'group',
+				'clone'  => true,
+				'sort_clone' => true,
+				'collapsible' => true,
+				'group_title' => array( 'field' => 'title' ), // ID of the subfield
+				'save_state' => true,
+				'fields' => array(
+					array(
+						'name' => __( 'Title', 'rwmb' ),
+						'id'   => $prefix . 'title',
+						'type' => 'text',
+					),
+					array(
+						'name' => __( 'Content', 'rwmb' ),
+						'id'   => $prefix . 'content',
+						'type' => 'wysiwyg',
+					),
+				),
+			),
+		),
+	);
+	return $meta_boxes;
+}
+?>
