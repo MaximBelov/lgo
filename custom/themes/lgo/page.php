be198c88005c1d845b5c7dfebc11926ea58130a1
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-31 15:56:43 -0400

    Updates type and styles

--- custom/themes/lgo/page.php
+++ custom/themes/lgo/page.php
@@ -28,30 +28,30 @@ $group_values = rwmb_meta( 'accordion_content' ); ?>
 	</div>
 <?php endif; ?>
 
-
-<section class="container section">
+<section class="container">
 	<div class="row">
-		<div class="col s12 m8 offset-m2">
-	 		
-            <?php the_content(); ?>
+		<div class="col s12 m10 offset-m1">
+			<?php the_content(); ?>
+		</div>
+	</div>
+</section>
 
-            <?php // Accordion
-            	if ( ! empty( $group_values ) ) { ?>
-				<ul class="collapsible" data-collapsible="accordion">
-				<?php foreach ( $group_values as $group_value ) {
-					$title 		= isset( $group_value['rw_title'] ) ? $group_value['rw_title'] : '';
-					$content 	= wpautop(isset( $group_value['rw_content'] ) ? $group_value['rw_content'] : ''); ?>
-    				<li>
-      				<div class="collapsible-header"><i class="material-icons right">add_circle_outline</i><?php echo $title; ?></div>
-      				<div class="collapsible-body"><span><?php echo $content; ?></span></div>
-    				</li>
-					<?php } //endforeach ?>
-				</ul>
-				<?php }; //endif !empty 
-			// end accordion ?>
+<section class="accordion-section">
+    <?php // Accordion
+    	if ( ! empty( $group_values ) ) { ?>
+		<ul class="collapsible" data-collapsible="accordion">
+		<?php foreach ( $group_values as $group_value ) {
+			$title 		= isset( $group_value['rw_title'] ) ? $group_value['rw_title'] : '';
+			$content 	= wpautop(isset( $group_value['rw_content'] ) ? $group_value['rw_content'] : ''); ?>
+			<li>
+				<div class="collapsible-header"><i class="material-icons right">add_circle_outline</i><?php echo $title; ?></div>
+				<div class="collapsible-body"><span><?php echo $content; ?></span></div>
+			</li>
+			<?php } //endforeach ?>
+		</ul>
+		<?php }; //endif !empty 
+	// end accordion ?>
 			
-        </div>
-    </div>
 </section>
 
 <?php endwhile; endif;  ?>
