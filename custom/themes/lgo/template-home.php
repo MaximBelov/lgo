e5a12970ae6f8f4505a858a6076d33ed463ddfb9
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-04-21 09:46:51 -0400

    Updates Home template

--- custom/themes/lgo/template-home.php
+++ custom/themes/lgo/template-home.php
@@ -27,59 +27,9 @@ $subhead   = rwmb_meta( 'rw_banner_subheading' );
 	</div>
 </div>
 
-<div class="left-panel__background left-panel__background---down">
-	
-</div>
+<div class="full-width-panel">
+<div> <!-- Start of RP -->
+<div> <!-- Start of RP inner -->
 
-<div class="right-panel">
 
-<?php if(have_posts()): while(have_posts()): the_post();
-
-// Get meta content and assign to variable 
-$group_values = rwmb_meta( 'accordion_content' ); ?>
-
-<?php if ( has_post_thumbnail() ) : ?>
-	<div class="parallax-container valign-wrapper">
-		<div class="container">
-			<div class="row">
-				<div class="col s12 m8 offset-m2">
-					<h1><?php the_title();?></h1>
-				</div>
-			</div>
-		</div>
-    	<div class="parallax overlay"><img src="<?php the_post_thumbnail_url(); ?>"></div>
-    </div>
-<?php elseif (!has_post_thumbnail() ) : ?>
-	<div id="skip-to-content">
-		<h4><?php the_title();?></h4>
-	</div>
-<?php endif; ?>
-
-<section class="container base-padding no-pad-bottom">
-	<div class="row">
-		<div class="col s12">
-			<?php the_content(); ?>
-		</div>
-	</div>
-</section>
-
-<section class="accordion-section base-padding no-padding-top">
-    <?php // Accordion
-    	if ( ! empty( $group_values ) ) { ?>
-		<ul class="collapsible" data-collapsible="accordion">
-		<?php foreach ( $group_values as $group_value ) {
-			$title 		= isset( $group_value['rw_title'] ) ? $group_value['rw_title'] : '';
-			$content 	= wpautop(isset( $group_value['rw_content'] ) ? $group_value['rw_content'] : ''); ?>
-			<li>
-				<div class="collapsible-header"><i class="material-icons right">add_circle_outline</i><?php echo $title; ?></div>
-				<div class="collapsible-body"><span><?php echo $content; ?></span></div>
-			</li>
-			<?php } //endforeach ?>
-		</ul>
-		<?php }; //endif !empty 
-	// end accordion ?>
-			
-</section>
-
-<?php endwhile; endif;  ?>
 <?php get_footer(); ?>
