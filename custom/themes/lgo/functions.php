7acd900f8e0dbc1f1b2d823bfc387f2bad57210a
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-05-09 11:05:21 -0400

    Updates functions.php and accordion styles

--- custom/themes/lgo/functions.php
+++ custom/themes/lgo/functions.php
@@ -10,7 +10,7 @@ require_once('includes/functions/custom-admin-branding.php');
 require_once('includes/functions/remove-admin-bar.php');
 require_once('includes/functions/remove-header-meta.php');
 // require_once('includes/functions/remove-menu-id.php');
-require_once('includes/functions/remove-wp-version.php');
+// require_once('includes/functions/remove-wp-version.php');
 require_once('includes/functions/grabfirstimage.php');
 require_once('includes/functions/TwitterAPIExchange.php');
 require_once('includes/functions/social-feed.php');
