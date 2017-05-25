6432bdeea94c98bba29c80e2da9294cbc3cf9044
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-05-25 15:38:49 -0400

    Adds Gravity Forms plugin

--- custom/themes/lgo/src/js/init.js
+++ custom/themes/lgo/src/js/init.js
@@ -144,9 +144,9 @@ jQuery(document).ready(function($) {
       $(this).next('ul').toggleClass( "open-dropdown" );
     });
 
-    // $('.menu-item-has-children').hover(function(){
-    //   $(this).children('ul').toggleClass( "open-dropdown" );
-    // });
+    $('.menu-item-has-children').hover(function(){
+      $(this).children('ul').toggleClass( "open-dropdown" );
+    });
 
     $('.menu-after-dots').keypress(function(e){
       if(e.which == 13){//Enter key pressed
