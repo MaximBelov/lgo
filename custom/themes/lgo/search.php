ba7852b3fdb9a6d3b22d1605a0850458c228fb7b
Lionel Mann <lionelmann@gmail.com>
2017-05-29 10:18:40 -0400

    search templates

---
+++ custom/themes/lgo/search.php
@@ -0,0 +1,34 @@
+<?php get_header();?>
+
+    <div class="wapper">
+      <div class="contentarea clearfix">
+        <div class="content">
+            <ul>
+                <?php if ( have_posts() ) : ?>
+
+            <header class="page-header">
+                <p><?php printf( __( 'Search Results for: %s', 'twentyfourteen' ), get_search_query() ); ?></p>
+            </header><!-- .page-header -->
+
+                        <?php
+                        // Start the Loop.
+                        while ( have_posts() ) : the_post();
+                        ?>
+                        <li><h3><a href="<?php get_the_permalink() ?>"><?php the_title(); ?></a></h3></li>
+                        <?php the_post_thumbnail('medium') ?>
+                        <?php echo substr(get_the_excerpt(), 0, 200); ?>
+                            <div class="h-readmore"> 
+                                <a href="<?php the_permalink(); ?>">Read More</a>
+                            </div>
+                        <?php
+                        endwhile;
+                else :
+                // If no content, include the "No posts found" template.
+                get_template_part( 'content', 'none' );
+                ?>       
+            </ul>                                 
+
+        </div>
+      </div>
+    </div>
+<?php get_footer(); ?>
---
+++ custom/themes/lgo/searchform.php
@@ -0,0 +1,4 @@
+<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+  <input type="search" id="searchbox" name="s" placeholder="Enter keywords" required>
+    <input type="image" id="searchsubmit" alt="Search" class="searchicon" src="" />
+</form>
