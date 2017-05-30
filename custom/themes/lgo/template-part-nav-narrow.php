bb5f9ad152672c88a011c1a06d8ddd1bc9743a64
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-05-30 17:31:02 -0400

    Updates nav

--- custom/themes/lgo/template-part-nav-narrow.php
+++ custom/themes/lgo/template-part-nav-narrow.php
@@ -43,6 +43,13 @@
                 'walker' => new Main_Menu_Walker()
                 )); 
             ?> 
+            <?php get_search_form(); ?>
+            <ul class="nav-social">
+                <li><a href="http://twitter.com/LGLizDowdeswell" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="http://facebook.com/LGLizDowdeswell" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+            </ul>
         </div>
     </nav>
 </div>
