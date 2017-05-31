157e2fa27f1cbaf6f661fd9eecbbc3de8f498c5e
Lionel Mann <lionelmann@gmail.com>
2017-05-31 10:15:54 -0400

    search form page

--- custom/themes/lgo/search.php
+++ custom/themes/lgo/search.php
@@ -1,34 +1,44 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-    <div class="wapper">
-      <div class="contentarea clearfix">
-        <div class="content">
-            <ul>
-                <?php if ( have_posts() ) : ?>
+<?php get_template_part( 'template-part-nav' ); ?>
 
-            <header class="page-header">
-                <p><?php printf( __( 'Search Results for: %s', 'lgo' ), get_search_query() ); ?></p>
-            </header><!-- .page-header -->
+<div id="skip-to-content" class="scroll-panel page-panel page__bg__fixed single-page-container--whole">
+    <div class="left-compartment__bg" style="background-image: url(<?php if ($thumbnail) { ?><?php echo $thumbnail[0]; ?><?php } else { echo get_template_directory_uri().'/src/images/background_default.svg'; } ?>);">
+    </div>
+    <div class="dark-overlay"></div>
+    <div class="right-compartment">
+        
+        <div class="single-page__content">
+        <h5>Search - <?php echo $wp_query->found_posts; ?> <?php _e( 'results for', 'locale' ); ?>: "<?php the_search_query(); ?>"</h5>
+        <?php // Loop starts
+        $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+        $s = get_search_query();
+        $args = array(
+            'paged' =>  $paged,
+            's' => $s
+        ); 
 
-                        <?php
-                        // Start the Loop.
-                        while ( have_posts() ) : the_post();
-                        ?>
-                        <li><h3><a href="<?php get_the_permalink() ?>"><?php the_title(); ?></a></h3></li>
-                        <?php the_post_thumbnail('medium') ?>
-                        <?php echo substr(get_the_excerpt(), 0, 200); ?>
-                            <div class="h-readmore"> 
-                                <a href="<?php the_permalink(); ?>">Read More</a>
-                            </div>
-                        <?php
-                        endwhile;
-                else :
-                // If no content, include the "No posts found" template.
-                //get_template_part( 'content', 'none' );
-                ?>       
-            </ul>                                 
+        $the_query = new WP_Query( $args ); 
+    ?>
+
+    <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+        <h4><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
+        <p><?php echo wp_trim_words( get_the_content(), 30, '...' );?><a href="<?php the_permalink(); ?>">read more</a></p>
+    
+    <?php endwhile; else: ?>
+
+        <h4>No Results</h4>
+        <p>Please feel free try again!</p>
+       
+    <?php endif;  ?>
+
+    <?php //pagination(); ?>
 
         </div>
-      </div>
-    </div>
+        
+    </div> <!-- END OF RIGHT COMPARTMENT -->
+</div> <!-- END OF .page -->
+
+
 <?php get_footer(); ?>
--- custom/themes/lgo/searchform.php
+++ custom/themes/lgo/searchform.php
@@ -1,4 +1,10 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+<!--<form action="/" id="searchform" method="get">
   <input type="search" id="searchbox" name="s" placeholder="Enter keywords" required>
     <input type="image" id="searchsubmit" alt="Search" class="searchicon" src="" />
+</form>
+-->
+
+<form id="searchform" method="get" action="<?php bloginfo('url'); ?>/" >
+<input type="text" class="searchbox" name="s" id="s" value="Search" onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search';}">
+<input type="submit" id="searchsubmit" style="display: none;" />
 </form>
