2beb04a75cf56cf7c508b076247ec37bd960e5c5
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-06-05 17:26:52 -0400

    Updates banners

--- custom/themes/lgo/template-splash-page.php
+++ custom/themes/lgo/template-splash-page.php
@@ -6,7 +6,7 @@ get_header(); ?>
 
 <?php if(have_posts()): while(have_posts()): the_post();
 
-$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'banner' ); 
+$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); 
 $random = rand(1,5);
 
 if ($random == 1) {
