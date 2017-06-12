fe61fdf4b80afbd875cd02e43f8110cce3a9034c
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-06-12 16:59:20 -0400

    Updates nav

--- custom/themes/lgo/template-part-nav-social.php
+++ custom/themes/lgo/template-part-nav-social.php
@@ -1,3 +1,11 @@
+<div class="nav-mailchimp">
+	<?php 
+	if(ICL_LANGUAGE_CODE=='fr'){ ?>
+		<a href="#">Souscrire</a>
+	<?php } else { ?>
+		<a href="#">Subscribe</a>
+	<?php } ?>
+</div>
 <ul class="nav-social">
 	<?php
 		if(get_option('lgo_option_name')){
@@ -17,9 +25,6 @@
 	?>
     <li><a href="http://twitter.com/<?php echo $twitter; ?>" target="_blank" alt="Twitter" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
     <li><a href="<?php echo $facebook; ?>" target="_blank" alt="Facebook" title="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-    <!-- <li><a href="https://www.instagram.com/<?php //echo $instagram; ?>" target="_blank" alt="Instagram" title="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li> -->
+    <li><a href="https://www.instagram.com/<?php echo $instagram; ?>" target="_blank" alt="Instagram" title="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
     <li><a href="<?php echo $youtube; ?>" target="_blank" alt="Youtube" title="Youtube"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
 </ul>
-<div class="nav-mailchimp">
-	<a href="#">Sign up for updates</a>
-</div>
