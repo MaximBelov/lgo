093376e1ad91fad223d342cfc9b79cb5e7ec1683
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-06-23 14:37:13 -0400

    Updates News template

--- custom/themes/lgo/template-news.php
+++ custom/themes/lgo/template-news.php
@@ -40,7 +40,7 @@ $subhead   = rwmb_meta( 'rw_banner_subheading' );
 					<!-- the loop -->
 					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
 						$tweetURL = get_post_meta( get_the_ID(), 'tweet_id', true);
-						$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'banner' ); 
+						$thumbnail =get_the_post_thumbnail_url(get_the_ID(), 'large' );
 						$random = rand(1,5);
 			
 						if ($random == 1) {
@@ -57,7 +57,7 @@ $subhead   = rwmb_meta( 'rw_banner_subheading' );
 						    $imgPath = '/src/images/banner_DukatPhotosLGOwineawards-2015.jpg';
 						}
 					?>
-						<a href="<?php the_permalink();?>" class="grid-item grid-item--2x2 wow fadeInUp" style="background-image: url(<?php if ($thumbnail) { ?><?php echo $thumbnail[0]; ?><?php } else { echo get_template_directory_uri().$imgPath; } ?>);">
+						<a href="<?php the_permalink();?>" class="grid-item grid-item--2x2 wow fadeInUp" style="background-image: url(<?php if (has_post_thumbnail()) { ?><?php echo $thumbnail; ?><?php } else { echo get_template_directory_uri().$imgPath; } ?>);">
 							<div class="grid-item--overlay"></div>
 							<div class="grid-item__wrapper">
 								<h3 class="grid-item--2x2--label"><?php the_title();?></h3>
