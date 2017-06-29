db6187f8c3eff3646ab28e6c218c62ed895e026e
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-06-29 12:04:09 -0400

    Updates default page temp and styles

--- custom/themes/lgo/page.php
+++ custom/themes/lgo/page.php
@@ -8,6 +8,22 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'ful
 $cta_copy   = rwmb_meta( 'rw_cta_blurb' );
 $cta_btns   = rwmb_meta( 'cta' );
 $subhead   = rwmb_meta( 'rw_banner_subheading' );
+
+$random = rand(1,5);
+
+if ($random == 1) {
+    $imgPath = '/src/images/banners/banner_AmbassadorsReception.jpg';
+} else if ($random == 2) {
+    $imgPath = '/src/images/banners/banner_DukatPhotosLGOwineawards-2015.jpg';
+} else if ($random == 3) {
+    $imgPath = '/src/images/banners/banner_LGO_reception.jpg';
+} else if ($random == 4) {
+    $imgPath = '/src/images/banners/banner_staircase.jpg';
+} else if ($random == 5) {
+    $imgPath = '/src/images/banners/banner_Worldpride-Reception.jpg';
+} else {
+    $imgPath = '/src/images/banner_DukatPhotosLGOwineawards-2015.jpg';
+}
 ?>
 
 <?php if ($cta_copy) { ?>
@@ -25,7 +41,7 @@ $subhead   = rwmb_meta( 'rw_banner_subheading' );
 			} ?>
 		</div>
 	</div>
-	<div class="left-panel" style="background-image: url(<?php if ($thumbnail) { ?><?php echo $thumbnail[0]; ?><?php } else { echo get_template_directory_uri().'/src/images/background_default.svg'; } ?>);background-size: cover;">
+	<div class="left-panel" style="background-image: url(<?php if ($thumbnail) { ?><?php echo $thumbnail[0]; ?><?php } else { echo get_template_directory_uri().$imgPath;} ?>);background-size: cover;">
 	</div><div class="right-panel">
 		<div class="single-page__content">
 			<h2><?php the_title();?></h2>
@@ -40,13 +56,13 @@ $subhead   = rwmb_meta( 'rw_banner_subheading' );
 <?php } else { ?>
 
 <div id="skip-to-content" class="scroll-panel page-panel page__bg__fixed single-page-container--whole">
-	<div class="left-compartment__bg" style="background-image: url(<?php if ($thumbnail) { ?><?php echo $thumbnail[0]; ?><?php } else { echo get_template_directory_uri().'/src/images/background_default.svg'; } ?>);">
+	<div class="left-compartment__bg" style="background-image: url(<?php if ($thumbnail) { ?><?php echo $thumbnail[0]; ?><?php } else { echo get_template_directory_uri().$imgPath; } ?>);">
 	</div>
 	<div class="dark-overlay"></div>
 	<div class="right-compartment">
 		
 		<div class="single-page__content">
-		<h2><?php the_title();?></h2>
+		<h1><?php the_title();?></h1>
 		<div class="single-lg__content-wysiwyg"><?php the_content(); ?></div>
 
 		<?php get_template_part( 'template-part-accordion' ); ?>
