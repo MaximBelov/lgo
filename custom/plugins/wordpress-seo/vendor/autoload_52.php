309554d5bc0e4c2385330133885799894ebe76f2
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-10-10 11:02:48 -0400

    Updates plugin - Yoast SEO v5.6

--- custom/plugins/wordpress-seo/vendor/autoload_52.php
+++ custom/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitcc1755b1d09f77a063058c1ec45890e6::getLoader();
+return ComposerAutoloaderInit2da3cc58521ce09b8a6716ef9756e74c::getLoader();
