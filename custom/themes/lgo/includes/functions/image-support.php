0cf88fa16dd6a4ffd169ad9199c9384f12e9eaa6
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-01-23 15:12:17 -0500

    Image crop update

--- custom/themes/lgo/includes/functions/image-support.php
+++ custom/themes/lgo/includes/functions/image-support.php
@@ -6,7 +6,7 @@
 
 add_theme_support( 'post-thumbnails' );
 add_image_size( 'banner', 1200, 800, true );
-add_image_size( 'event-card', 500, 620, true );
+add_image_size( 'event-card', 500, 620);
 
 
 add_filter( 'image_size_names_choose', 'wpshout_custom_sizes' );
