9906787e721c02984a16cc8fc11352e7d3268e1a
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-01-24 13:38:39 -0500

    Changes default Yoast og:image tag

--- custom/themes/lgo/functions.php
+++ custom/themes/lgo/functions.php
@@ -21,6 +21,74 @@ require_once('includes/metaboxio/lg-meta-fields.php');
 // Adds General Settings Fields
 require_once('includes/functions/add-settings.php');
 
+// Yoast Changes
+add_action( 'wpseo_opengraph', 'my_gv_wpseo_opengraph_image', 10, 1);
+function my_gv_wpseo_opengraph_image( $img ) {
+    
+    if( is_single()) {
+        $FBCrop = wp_get_attachment_image( get_post_thumbnail_id( get_the_ID() ), 'facebook' );
+        if($FBCrop != false) {
+            $FBCard = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID() ), 'facebook' );
+            // $FBThumbnail = $FBCard[0];
+            $img = $FBCard[0];
+        } else {
+            $img = get_the_post_thumbnail_url(get_the_ID(), 'large' );
+        }
+    } else {
+        return;
+    }
+
+    $GLOBALS['wpseo_og']->image_output( $img ); 
+}
+
+// function enable_wpseo_twitter_image(){
+//     if(is_single()){
+//         $object = get_queried_object();
+//         if($object && isset($object->name)){
+//             $ID = getRelatedPageForCPT($object->name);
+//             //first check if yoast seo metabox contains the image url
+//             if($ID && get_post_meta($ID,'_yoast_wpseo_opengraph-image', true)){
+//                 $img = get_post_meta($ID,'_yoast_wpseo_opengraph-image', true);
+//                 $property = 'twitter:image';
+//                 $content = $img[0];
+//                 echo '<meta name="', esc_attr( $property ), '" content="', esc_attr( $content ), '" />', "\n";
+//                 return;
+//             }
+//             if($ID && get_post_thumbnail_id($ID) != ''){
+//                 $img = wp_get_attachment_image_src(get_post_thumbnail_id($ID),'full');
+//                 $property = 'twitter:image';
+//                 $content = $img[0];
+//                 echo '<meta name="', esc_attr( $property ), '" content="', esc_attr( $content ), '" />', "\n";
+//                 return;
+//             }
+//         }
+//         $options = get_site_option('wpseo_social',null);
+//         if($options && is_array($options)){
+//             $property = 'twitter:image';
+//             $content = $options['og_default_image'];
+//             echo '<meta name="', esc_attr( $property ), '" content="', esc_attr( $content ), '" />', "\n";
+//         }
+//     }
+// }
+
+// add_action( 'wpseo_twitter_image', 'my_gv_wpseo_twitter_image', 10, 1);
+// function my_gv_wpseo_twitter_image( $img ) {
+    
+//     if( is_single()) {
+//         $TwitterCrop = wp_get_attachment_image( get_post_thumbnail_id( get_the_ID() ), 'twitter' );
+//         if($TwitterCrop != false) {
+//             $TwitterCard = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID() ), 'twitter' );
+//             $img = 'https://google.com/twitter.png';
+//         } else {
+//             $img = 'https://google.com/twitter.png';
+//         }
+//     } else {
+//         return;
+//     }
+
+//     $GLOBALS['wpseo_twitter_image']->image_output( $img ); 
+// }
+
 // WPML Custom Code
 // add_filter('icl_ls_languages', 'wpml_ls_filter');
 // function wpml_ls_filter($languages) {
